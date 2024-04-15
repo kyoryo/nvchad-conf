2bace633d38b09501ffaae65453966cd8d5078b2
Kyoryo Ishigami <franz.rio19@gmail.com>
2024-04-15 14:58:52 +0700

    init

---
+++ lua/plugins/init.lua
@@ -0,0 +1,56 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- lua
+        "lua-language-server",
+        "stylua",
+        -- webdev
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        -- golang
+        "gopls",
+        "gofumpt",
+        "goimports-reviser",
+        "golines",
+        "go-debug-adapter",
+        -- protobuf
+        "buf-language-server",
+        "buf",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+      },
+    },
+  },
+  -- {
+  --   "christoomey/vim-tmux-navigator",
+  --   lazy = false,
+  -- },
+}
