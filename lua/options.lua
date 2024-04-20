0a81d27871e34e2b83e9853af8acc271a158f9d7
Kyoryo Ishigami <franz.rio19@gmail.com>
2024-04-21 00:06:44 +0700

    add highlight yank

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,12 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", {}),
+  desc = "Hightlight selection on yank",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank { higroup = "IncSearch", timeout = 100 }
+  end,
+})
