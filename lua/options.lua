e11005e4d8a1054abbbe6fc8ee360064f70b6e5a
kyoryo <franz.rio19@gmail.com>
2024-04-28 16:55:22 +0700

    remove golang dap adapter

--- lua/options.lua
+++ lua/options.lua
@@ -5,6 +5,12 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
+-- set default relative number
+vim.opt.relativenumber = true
+-- Sync clipboard between OS and Neovim.
+vim.opt.clipboard = "unnamedplus"
+
+-- highlight yank
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("highlight_yank", {}),
   desc = "Hightlight selection on yank",
--- lua/plugins/golang.lua
+++ lua/plugins/golang.lua
@@ -1,14 +1,5 @@
 local golang = {
   {
-    "leoluz/nvim-dap-go",
-    ft = "go",
-    dependencies = "mfussenegger/nvim-dap",
-    config = function(_, opts)
-      require("dap-go").setup(opts)
-      -- require("core.utils").load_mappings "dap_go"
-    end,
-  },
-  {
     "olexsmir/gopher.nvim",
     ft = "go",
     dependencies = {
