ef56541793d5a1d3ad0134b3cd1d1797ec5c76f6
kyoryo <franz.rio19@gmail.com>
2024-04-28 16:52:57 +0700

    remove jk mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,7 +5,7 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>", { desc = "type jk in insert mode to esc" })
+-- map("i", "jk", "<ESC>", { desc = "type jk in insert mode to esc" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
@@ -42,16 +42,16 @@ map("n", "<C-S-N>", function()
 end, { desc = "harpoon list, prev buff" })
 
 -- debug keymap
-map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Debug, add breakpoint at line" })
-map("n", "<leader>dus", function()
-  local widgets = require "dap.ui.widgets"
-  local sidebar = widgets.sidebar(widgets.scopes)
-  sidebar.open()
-end, { desc = "Debug, open sidebar" })
-
-map("n", "<leader>dgt", function()
-  require("dap-go").debug_test()
-end, { desc = "Debug, debug go test" })
-map("n", "<leader>dgl", function()
-  require("dap-go").debug_last()
-end, { desc = "Debug, debug last go test" })
+-- map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Debug, add breakpoint at line" })
+-- map("n", "<leader>dus", function()
+--   local widgets = require "dap.ui.widgets"
+--   local sidebar = widgets.sidebar(widgets.scopes)
+--   sidebar.open()
+-- end, { desc = "Debug, open sidebar" })
+--
+-- map("n", "<leader>dgt", function()
+--   require("dap-go").debug_test()
+-- end, { desc = "Debug, debug go test" })
+-- map("n", "<leader>dgl", function()
+--   require("dap-go").debug_last()
+-- end, { desc = "Debug, debug last go test" })
