838b0abd10758e468c75b859fa9bf83044c24fb1
kyoryo <franz.rio19@gmail.com>
2024-11-16 03:15:45 +0700

    move deprecated lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "bufls" }
+local servers = { "html", "cssls", "buf_ls" }
 local util = require "lspconfig/util"
 
 -- lsps with default config
@@ -17,7 +17,7 @@ for _, lsp in ipairs(servers) do
 end
 
 -- typescript
-lspconfig.tsserver.setup {
+lspconfig.ts_ls.setup {
   on_attach = on_attach,
   on_init = on_init,
   capabilities = capabilities,
