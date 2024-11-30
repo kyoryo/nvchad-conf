9ed8bccfd85cf731c0a1d66a93070d8ccf7f783d
kyoryo <franz.rio19@gmail.com>
2024-11-30 18:35:27 +0700

    add fzf mapping

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -12,5 +12,25 @@ M.base46 = {
   --   -- 	["@comment"] = { italic = true },
   --   -- },
 }
+M.ui = {
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "default",
+    order = nil,
+    modules = nil,
+  },
+  -- lazyload it when there are 1+ buffers
+  tabufline = {
+    enabled = true,
+    lazyload = true,
+    order = { "treeOffset", "buffers", "tabs", "btns" },
+    modules = nil,
+  },
+}
+M.nvdash = {
+  load_on_startup = true,
+}
 
 return M
--- lua/configs/fzf.lua
+++ lua/configs/fzf.lua
@@ -1,3 +1,10 @@
-local options = {}
+local options = {
+
+  defaults = {
+    git_icon = false,
+    file_icon = false,
+    color_icon = false,
+  },
+}
 
 require("fzf-lua").setup(options)
