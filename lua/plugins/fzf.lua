4a353557ed0c1a7d3f938f5f65a0b0d5d0af3372
kyoryo <franz.rio19@gmail.com>
2024-12-01 23:35:33 +0700

    use configs folder on fzf

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -4,8 +4,7 @@ local options = {
     -- optional for icon support
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      -- calling `setup` is optional for customization
-      require("fzf-lua").setup {}
+      require "configs.fzf"
     end,
   },
 }
