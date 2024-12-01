d4aac2328b4e74f49881081255becae7f6be4db3
kyoryo <franz.rio19@gmail.com>
2024-12-01 17:52:36 +0700

    add move folding to the left

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -35,10 +35,10 @@ return {
               "toggleterm",
             },
             segments = {
-              { text = { "%s" }, click = "v:lua.ScSa" },
-              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
               { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
               { text = { " " } }, -- separator
+              { text = { "%s" }, click = "v:lua.ScSa" },
+              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
             },
           }
         end,
