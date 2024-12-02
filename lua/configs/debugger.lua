b05af0696cc75511ca177e84d0d93fce55507222
kyoryo <franz.rio19@gmail.com>
2024-12-02 23:29:46 +0700

    move debugger configuration to  configs/ debugger

---
+++ lua/configs/debugger.lua
@@ -0,0 +1,29 @@
+local M = {}
+local dap_go = require "configs.debugger.go"
+
+function M.setup_dap_and_dapui(opts)
+  local dap = require "dap"
+  local dapui = require "dapui"
+  dap.set_log_level "INFO"
+  dapui.setup(opts)
+
+  dap.listeners.before.attach.dapui_config = function()
+    dapui.open {}
+  end
+  dap.listeners.before.launch.dapui_config = function()
+    dapui.open {}
+  end
+  dap.listeners.before.event_terminated.dapui_config = function()
+    dapui.close {}
+  end
+  dap.listeners.before.event_exited.dapui_config = function()
+    dapui.close {}
+  end
+end
+
+function M.setup(opts)
+  M.setup_dap_and_dapui(opts)
+  dap_go.setup_adapter()
+end
+
+return M
