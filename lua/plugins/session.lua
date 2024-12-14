b8ff88b8310f9bcdcac3e5769faf4baf5217e7ae
kyoryo <franz.rio19@gmail.com>
2024-12-15 04:18:37 +0700

    add session logic to restore nvimtree

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -10,14 +10,51 @@ return {
       end,
       suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/", "~/git" },
       -- log_level = "error",
-      pre_save_cmds = { "NvimTreeClose" },
+      pre_save_cmds = {
+        -- "NvimTreeClose"
+        function()
+          local status_ok, api = pcall(require, "nvim-tree.api")
+          if not status_ok then
+            return
+          end
+          local ok, view = pcall(require, "nvim-tree.view")
+          if not ok then
+            return
+          end
+          if view.is_visible() then
+            api.tree.close()
+          end
+        end,
+      },
+      post_save_cmds = {
+        function()
+          local status_ok, api = pcall(require, "nvim-tree.api")
+          if not status_ok then
+            return
+          end
+          api.tree.close()
+          -- api.tree.toggle { focus = false, find_file = true }
+        end,
+      },
       post_restore_cmds = {
         function()
-          -- Restore nvim-tree after a session is restored
-          local nvim_tree_api = require "nvim-tree.api"
-          nvim_tree_api.tree.open()
-          nvim_tree_api.tree.change_root(vim.fn.getcwd())
-          nvim_tree_api.tree.reload()
+          local status_ok, api = pcall(require, "nvim-tree.api")
+          if not status_ok then
+            return
+          end
+          local ok, view = pcall(require, "nvim-tree.view")
+          if not ok then
+            return
+          end
+          for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            local name = vim.api.nvim_buf_get_name(buf)
+            if name:match "NvimTree*" then
+              if not view.is_visible() then
+                api.tree.toggle { focus = false, find_file = true }
+              end
+              break
+            end
+          end
         end,
       },
     }
