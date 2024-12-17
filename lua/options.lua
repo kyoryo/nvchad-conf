336aa076668503ffa2059131ba507acdc3f2382b
kyoryo <franz.rio19@gmail.com>
2024-12-18 00:51:59 +0700

    change diagnostic icons

--- lua/options.lua
+++ lua/options.lua
@@ -7,6 +7,8 @@ require "nvchad.options"
 
 -- set default relative number
 vim.opt.relativenumber = true
+vim.opt.colorcolumn = "120"
+
 -- Sync clipboard between OS and Neovim.
 vim.opt.clipboard = "unnamedplus"
 vim.opt.shiftwidth = 4
@@ -44,6 +46,24 @@ vim.fn.sign_define(
   -- { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
   { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
 )
+
+-- local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- Show line diagnostics automatically in hover window
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+-- vim.o.updatetime = 250
+-- vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+--   group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+--   callback = function()
+--     vim.diagnostic.open_float(nil, { focus = false })
+--   end,
+-- })
 
 -- vim.fn.sign_define(
 --   "DapBreakpointCondition",
