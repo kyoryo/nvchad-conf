525fcb9e54dbb4869937ac3b0bf84ac675f82428
kyoryo <franz.rio19@gmail.com>
2024-12-25 17:45:40 +0700

    adjust scroll multiplier

--- lua/plugins/eyecandy.lua
+++ lua/plugins/eyecandy.lua
@@ -26,10 +26,10 @@ return {
         mappings = { -- Keys to be mapped to their corresponding default scrolling animation
           -- "<C-u>",
           -- "<C-d>",
-          "<C-b>",
-          "<C-f>",
-          "<C-y>",
-          "<C-e>",
+          -- "<C-b>",
+          -- "<C-f>",
+          -- "<C-y>",
+          -- "<C-e>",
           "zt",
           "zz",
           "zb",
@@ -38,7 +38,8 @@ return {
         stop_eof = true, -- Stop at <EOF> when scrolling downwards
         respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
         cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-        easing = "linear", -- Default easing function
+        easing = "sine", -- Default easing function
+        duration_multiplier = 1.0, -- Global duration multiplier
         pre_hook = nil, -- Function to run before the scrolling animation starts
         post_hook = nil, -- Function to run after the scrolling animation ends
         performance_mode = false, -- Disable "Performance Mode" on all buffers.
