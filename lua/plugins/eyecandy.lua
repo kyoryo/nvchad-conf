7b091d0ebb1145c7c3c2267e307c5969195d2876
kyoryo <franz.rio19@gmail.com>
2024-12-26 23:16:15 +0700

    markview lazyload on ft

--- lua/plugins/eyecandy.lua
+++ lua/plugins/eyecandy.lua
@@ -116,8 +116,8 @@ return {
   },
   {
     "OXY2DEV/markview.nvim",
-    lazy = false,
-    -- ft = "markdown",
+    -- lazy = false,
+    ft = "markdown",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons",
