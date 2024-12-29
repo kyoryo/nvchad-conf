e890b42fe5d7697c8a8677d930d78df220cb2e4f
kyoryo <franz.rio19@gmail.com>
2024-12-29 23:16:00 +0700

    add localleader

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
+vim.g.maplocalleader = "<C-s>"
 
 vim.g.nvim_tree_respect_buf_cwd = 0
 -- bootstrap lazy and all plugins
