bd09efcfbd46ec60ec926dae2834825db10f4801
kyoryo <franz.rio19@gmail.com>
2025-01-04 14:51:54 +0700

    add swagger and yaml

---
+++ lua/plugins/yaml.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "someone-stole-my-name/yaml-companion.nvim",
+    dependencies = {
+      { "neovim/nvim-lspconfig" },
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    enable = false,
+    config = function()
+      require("telescope").load_extension "yaml_schema"
+    end,
+  },
+}
