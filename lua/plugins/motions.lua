600a575b4dc3e19db168a52f4277f2074c20d78b
kyoryo <franz.rio19@gmail.com>
2025-01-11 15:14:43 +0700

    add demicolon

--- lua/plugins/motions.lua
+++ lua/plugins/motions.lua
@@ -67,4 +67,13 @@ return {
       }
     end,
   },
+  {
+    "mawkler/demicolon.nvim",
+    keys = { ";", ",", "t", "f", "T", "F", "]", "[", "]d", "[d" }, -- Uncomment this to lazy load
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    opts = {},
+  },
 }
