a01b1e43a6e53b7be031ca373dd4d8e6b9cc6682
kyoryo <franz.rio19@gmail.com>
2025-02-03 03:42:16 +0700

    open dapui in new tab

--- lua/configs/debugger.lua
+++ lua/configs/debugger.lua
@@ -8,18 +8,44 @@ function M.setup_dap_and_dapui(opts)
   dap.set_log_level "INFO"
   dapui.setup(opts)
 
-  -- automaticly open and close dap ui
+  -- Check if opts.open_in_tab is set, default to false
+  local open_in_tab = true
+  local dapui_tab = nil -- Store the tab number where DAP UI is opened
+
+  local function open_dapui()
+    if open_in_tab then
+      vim.cmd "tabnew"
+      dapui_tab = vim.fn.tabpagenr() -- Store the newly opened tab number
+      vim.cmd "lua require('dapui').open()"
+    else
+      dapui.open {}
+    end
+  end
+
+  local function close_dapui()
+    if open_in_tab and dapui_tab then
+      if vim.fn.tabpagenr() == dapui_tab then
+        vim.cmd "tabclose"
+      else
+        vim.cmd(dapui_tab .. "tabnext | tabclose")
+      end
+      dapui_tab = nil -- Reset after closing
+    else
+      dapui.close {}
+    end
+  end
+
   dap.listeners.before.attach.dapui_config = function()
-    dapui.open {}
+    open_dapui()
   end
   dap.listeners.before.launch.dapui_config = function()
-    dapui.open {}
+    open_dapui()
   end
   dap.listeners.before.event_terminated.dapui_config = function()
-    dapui.close {}
+    close_dapui()
   end
   dap.listeners.before.event_exited.dapui_config = function()
-    dapui.close {}
+    close_dapui()
   end
 end
 
