87288b4317a1007da398dcb3dcf349e40edb5c34
kyoryo <franz.rio19@gmail.com>
2025-02-08 04:13:51 +0700

    use profile instead of custom mapping

--- lua/configs/fzf.lua
+++ lua/configs/fzf.lua
@@ -1,27 +1,32 @@
 local options = {
+  {
+    -- fzf profile and fallbacks
+    "telescope",
+    -- "fzf-native",
+  },
+  fzf_opts = { ["--layout"] = "reverse" },
+  winopts = {},
   defaults = {
     git_icon = false,
     file_icon = false,
     color_icon = false,
   },
-  keymap = {
-    builtin = {
-      ["<C-d>"] = "preview-page-down",
-      ["<C-u>"] = "preview-page-up",
-    },
-  },
-  files = {
-    actions = {
-      -- "+accept" isn't required since `accept` is implied with actions
-      ["ctrl-q"] = { fn = require("fzf-lua").actions.file_sel_to_qf, prefix = "select-all" },
-    },
-  },
-  grep = {
-    actions = {
-      -- "+accept" isn't required since `accept` is implied with actions
-      ["ctrl-q"] = { fn = require("fzf-lua").actions.file_sel_to_qf, prefix = "select-all" },
-    },
-  },
+
+  -- uncomment to use custom
+  -- files = {
+  --   actions = {
+  --     -- true, -- uncomment to inherit all default config in custom config
+  --     -- "+accept" isn't required since `accept` is implied with actions
+  --     ["ctrl-q"] = { fn = require("fzf-lua").actions.file_sel_to_qf, prefix = "select-all" },
+  --   },
+  -- },
+  -- grep = {
+  --   actions = {
+  --     -- true, -- uncomment to inherit all default config in custom config
+  --     -- "+accept" isn't required since `accept` is implied with actions
+  --     ["ctrl-q"] = { fn = require("fzf-lua").actions.file_sel_to_qf, prefix = "select-all" },
+  --   },
+  -- },
 }
 
 require("fzf-lua").setup(options)
