1c49951ca6f58a7ff374730871ee75be3462363e
kyoryo <franz.rio19@gmail.com>
2025-02-15 10:25:11 +0700

    add namu symbol search

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -15,4 +15,64 @@ return {
       }
     end,
   },
+  {
+    "bassamsdata/namu.nvim",
+    lazy = false,
+    config = function()
+      require("namu").setup {
+        -- Enable the modules you want
+        namu_symbols = {
+          enable = true,
+          options = {
+            AllowKinds = {
+              default = {
+                "Function",
+                "Method",
+                "Class",
+                "Module",
+                "Property",
+                "Variable",
+              },
+              go = {
+                "Function",
+                "Method",
+                -- "Struct",
+                -- "Field",
+                "Interface",
+                "Constant",
+                "Property",
+              },
+              lua = { "Function", "Method", "Table", "Module" },
+              python = { "Function", "Class", "Method" },
+              yaml = { "Object", "Array" },
+              json = { "Module" },
+              toml = { "Object" },
+              markdown = { "String" },
+            },
+          }, -- here you can configure namu
+        },
+        -- Optional: Enable other modules if needed
+        colorscheme = {
+          enable = false,
+          options = {
+            -- NOTE: if you activate persist, then please remove any vim.cmd("colorscheme ...") in your config, no needed anymore
+            persist = true, -- very efficient mechanism to Remember selected colorscheme
+            write_shada = false, -- If you open multiple nvim instances, then probably you need to enable this
+          },
+        },
+        ui_select = { enable = false }, -- vim.ui.select() wrapper
+      }
+      -- === Suggested Keymaps: ===
+      local namu = require "namu.namu_symbols"
+      -- local colorscheme = require "namu.colorscheme"
+      vim.keymap.set("n", "<leader>nss", namu.show, {
+        desc = "Jump to LSP symbol",
+        silent = true,
+      })
+      -- vim.keymap.set("n", "<leader>nth", colorscheme.show, {
+      --   desc = "Colorscheme Picker",
+      --   silent = true,
+      -- })
+    end,
+  },
 }
