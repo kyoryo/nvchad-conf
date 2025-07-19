fa295990832ce86f59e7087c1c5edebe44dd1551
kyoryo <franz.rio19@gmail.com>
2025-07-19 13:32:28 +0700

    update chadrc and lazylock

--- lua/plugins/golang.lua
+++ lua/plugins/golang.lua
@@ -8,6 +8,8 @@ local golang = {
   config = function()
     require("go").setup {
       tag_transform = "camelcase", -- set to e.g. 'snakecase' to transform to snake_case
+      -- verbose = true, -- enable for debug
+      -- log_path = vim.fn.expand "$HOME" .. "/tmp/gonvim.log", -- this put lots at ~/tmp/gonvim.log
     }
   end,
   ft = { "go", "gomod" },
