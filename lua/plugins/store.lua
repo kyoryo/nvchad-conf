954cfdf8a1a89d7b7368efcad3ec815c0b8020d8
kyoryo <franz.rio19@gmail.com>
2025-07-20 00:51:17 +0700

    add store nvim

---
+++ lua/plugins/store.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "alex-popov-tech/store.nvim",
+    dependencies = {
+      "OXY2DEV/markview.nvim", -- optional, for pretty readme preview / help window
+    },
+    cmd = "Store",
+    keys = {
+      { "<leader><leader>st", "<cmd>Store<cr>", desc = "Open Plugin Store" },
+    },
+    opts = {
+      -- optional configuration here
+    },
+  },
+}
