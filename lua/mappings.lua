55a04d386095cbb1ad0c60ca57a0e038cd321d49
kyoryo <franz.rio19@gmail.com>
2025-07-23 23:54:08 +0700

    limit golang specific shortcut

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -29,17 +29,23 @@ local map = vim.keymap.set
 --
 map("n", "<localleader>ttt", ":echo 'hello' <cr>") -- test
 
--- go
-map("n", "<localleader>tf", ":GoTestFunc -Fv<cr>", { desc = "Go Test Func" })
-map("n", "<localleader>tF", ":GoTestFile -Fv<cr>", { desc = "Go Test File" })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    -- go
+    map("n", "<localleader>tf", ":GoTestFunc -Fv<cr>", { desc = "Go Test Func" })
+    map("n", "<localleader>tF", ":GoTestFile -Fv<cr>", { desc = "Go Test File" })
 
--- dap go
-map("n", "<localleader>dgt", function()
-  require("dap-go").debug_test()
-end, { desc = "DEBUG debug go test" })
-map("n", "<localleader>dgl", function()
-  require("dap-go").debug_last_test()
-end, { desc = "DEBUG debug last go test" })
+    -- dap go
+    map("n", "<localleader>dgt", function()
+      require("dap-go").debug_test()
+    end, { desc = "DEBUG debug go test" })
+    map("n", "<localleader>dgl", function()
+      require("dap-go").debug_last_test()
+    end, { desc = "DEBUG debug last go test" })
+  end,
+})
+
 -- ////////////////////////////////////////////
 -- END local leader (conditional command mapping)
 -- ////////////////////////////////////////////
