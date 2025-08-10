5f3cd04c0b0e2367133500083b5d9726392ccb92
kyoryo <franz.rio19@gmail.com>
2025-08-11 01:21:17 +0700

    grammar?

--- lua/plugins/diagnostics.lua
+++ lua/plugins/diagnostics.lua
@@ -1,6 +1,6 @@
 return {
   {
-    -- this possible archived because nvim has built-in workspace diagnostics
+    -- this is possibly to be archived because nvim has built-in workspace diagnostics
     "artemave/workspace-diagnostics.nvim",
     event = "BufRead",
     config = function()
