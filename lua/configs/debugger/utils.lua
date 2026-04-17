6680d4c281526cdc9825683c82f579f1a7253556
kyoryo <franz.rio19@gmail.com>
2026-04-17 11:50:18 +0700

    detect method in golang when debugging

--- lua/configs/debugger/utils.lua
+++ lua/configs/debugger/utils.lua
@@ -4,10 +4,18 @@ function M.get_nearest_test()
   local line = vim.fn.line "."
   for l = line, 1, -1 do
     local text = vim.fn.getline(l)
+
     -- Look for a Go test function
-    local name = text:match "^func%s+(Test%w+)"
-    if name then
-      return name
+    -- Match: func TestXxxx
+    local test_name = text:match "^func%s+(Test%w+)"
+    if test_name then
+      return test_name
+    end
+
+    -- Match: func (r *Type) MethodName
+    local method_name = text:match "^func%s*%b()%s*(%w+)"
+    if method_name then
+      return method_name
     end
   end
   return nil
