df11026ba930affd544e6f7b6edeb7661cebaf98
kyoryo <franz.rio19@gmail.com>
2026-04-17 11:50:38 +0700

    add telescope mapping

--- lua/telescope_mappings.lua
+++ lua/telescope_mappings.lua
@@ -4,11 +4,13 @@ function M.live_grep_mappings(_, map)
   local actions = require "telescope.actions"
   local lga_actions = require "telescope-live-grep-args.actions"
 
-  map("i", "<C-k>", lga_actions.quote_prompt(), { desc = "Quote prompt asrtrast" })
+  map("i", "<C-a>", lga_actions.quote_prompt(), { desc = "Quote prompt" })
+  map("i", "<C-g>", lga_actions.quote_prompt { postfix = " --iglob " }, { desc = "Quote + iglob" })
+  map("i", "<C-t>", lga_actions { postfix = " --type " }, { desc = "Quote + type" })
   map("i", "<C-space>", actions.to_fuzzy_refine, { desc = "Fuzzy refine results" })
-  -- map("i", "<C-i>", lga_actions.quote_prompt { postfix = " --iglob " }, { desc = "Quote + iglob" })
-  map("i", "<C-i>", lga_actions.quote_prompt { postfix = " --iglob " }, { desc = "Quote + iglob" })
-  map("i", "<C-t><C-t>", lga_actions.quote_prompt { postfix = " --type " }, { desc = "Quote + type" })
+  map("i", "<C-k>", actions.cycle_history_prev, { desc = "Cycle history prev" })
+  map("i", "<C-j>", actions.cycle_history_next, { desc = "Cycle history next" })
+
   return true
 end
 
