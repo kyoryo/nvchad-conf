edf49b813030a39a0cea40c94a7fdb4b4d53ecd0
kyoryo <franz.rio19@gmail.com>
2026-04-19 21:52:42 +0700

    re override mapping

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -14,12 +14,19 @@ return {
   {
     "otavioschwanck/github-pr-reviewer.nvim",
     opts = {
-      -- options here
       show_inline_diff = false,
     },
     keys = {
       { "<leader>gr", "<cmd>PRReviewMenu<cr>", desc = "PR Review Menu" },
       { "<leader>gr", ":<C-u>'<,'>PRSuggestChange<CR>", desc = "Suggest change", mode = "v" },
     },
+    config = function(_, opts)
+      require("github-pr-reviewer").setup(opts)
+
+      vim.keymap.del("n", "<C-e>")
+      vim.keymap.set("n", "<C-e>", function()
+        require("harpoon").ui:toggle_quick_menu(require("harpoon"):list())
+      end, { desc = "HARPOON open menu" })
+    end,
   },
 }
