7f220822882e77602c7f2bd0d2535310c57d104d
kyoryo <franz.rio19@gmail.com>
2026-09-16 13:20:43 +0700

    update motion

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -2,7 +2,8 @@ return {
   {
     -- POSTMAN/INSOMNIA REST CLIENT
     "mistweaverco/kulala.nvim",
-    -- commit = "6656c9d332735ca6a27725e0fb45a1715c4372d9",
+    commit = "6656c9d332735ca6a27725e0fb45a1715c4372d9", -- old vanilla kulala
+    -- commit = "a9e17ae0236e886371671a7a9f01f070201a9cc1",
     keys = {
       { "<leader>Rs", desc = "Send request" },
       { "<leader>Ra", desc = "Send all requests" },
